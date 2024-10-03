f4949f0abc5d19fffb3361a8f522b8e08f8dd58e
fifazii91 <fifa.settwut@gmail.com>
2024-10-03 14:39:54 +0700

    feat: Add JavaScript Equality of Values Assignment

--- ex-1.js
+++ ex-1.js
@@ -4,11 +4,11 @@ Object.is(10, 20); //false
 Object.is(0, -0); //false
 Object.is(NaN, 100); //false
 Object.is("one", "two"); //false
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //true
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false
 Object.is(null, undefined); //false
 
 10 === 20; //false
-10 === "10" //false
+10 === "10"; //false
 3.14 !== 3.14; //false
 "TechUp" !== "TechUp"; //false
 NaN === NaN; //false
